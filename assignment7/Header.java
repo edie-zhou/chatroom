e4bcef792dd1fdc474cf6fa55728c29f44bea625
Edie Zhou <edie198@gmail.com>
2019-11-25 23:54:48 -0600

    Initial commit for package and Header

---
+++ assignment7/Header.java
@@ -0,0 +1,18 @@
+/* CHAT ROOM <MyClass.java>
+ * EE422C Project 7 submission by
+ * Replace <...> with your actual data.
+ * Edie Zhou
+ * ez3437
+ * 16180
+ * Adit Jain
+ * aj27923 
+ * 16180
+ * Slip days used: 1
+ * Fall 2019
+ */
+
+/*
+   Describe here known bugs or issues in this file. If your issue spans multiple
+   files, or you are not sure about details, add comments to the README.txt file.
+ */
+package assignment7;
